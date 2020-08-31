6a93b5fa9aa60d8dd2ae8caae50daf92d9c0612d
Курганников Андрей Алексеевич <kurgannikov.andrey@bk.ru>
2020-08-31 23:30:41 +0300

    first commit

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1920;
+		config.height = 1080;
+		config.title = "Your dungeon";
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }

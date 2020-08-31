39b941e97c31f9c9762196b334898170ba8b759d
Nondefraudat <60321109+Nondefraudat@users.noreply.github.com>
2020-08-31 23:33:41 +0300

    nothing

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1920;
 		config.height = 1080;
-		config.title = "Your dungeon";
+		config.title = "Your Dungeon";
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }

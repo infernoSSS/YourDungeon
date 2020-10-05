5b51760a7ad6b890e94a955686f17ee54205118c
Курганников Андрей Алексеевич <kurgannikov.andrey@bk.ru>
2020-10-05 22:42:48 +0300

    Вывод меню

---
+++ core/src/com/mygdx/game/objects/Ship.java
@@ -0,0 +1,27 @@
+package com.mygdx.game.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+import com.mygdx.game.managers.GameManager;
+
+public class Ship extends GameObject {
+    @Override
+    public void create(GameManager gameManager, Vector3 position) {
+        super.create(gameManager, position);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        super.draw(batch);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
--- core/src/com/mygdx/game/scenes/GameScene.java
+++ core/src/com/mygdx/game/scenes/GameScene.java
@@ -2,11 +2,22 @@ package com.mygdx.game.scenes;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.managers.GameManager;
+import com.mygdx.game.objects.GameObject;
+import com.mygdx.game.objects.Ship;
+
+import java.util.ArrayList;
 
 public class GameScene extends Scene {
+    ArrayList<GameObject> shipList;
+
     @Override
     public void create(GameManager gameManager) {
         super.create(gameManager);
+
+        shipList = new ArrayList<>();
+        shipList.add(new Ship());
+
+        Lists.add(shipList);
     }
 
     @Override

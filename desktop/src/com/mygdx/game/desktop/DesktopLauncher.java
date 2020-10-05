e881c60173080eee4dd09aa88df9ad37192326b4
Nondefraudat <60321109+Nondefraudat@users.noreply.github.com>
2020-10-05 17:15:57 +0300

    Merge branch 'master' of D:\DEVData\IntelliJ\YourDungeon with conflicts.

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,11 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) { //Hellow
+	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1920;
 		config.height = 1080;
-		config.title = "Star Cripple";
+		config.title = "Your Dungeon";
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
